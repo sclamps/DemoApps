8877352ac8964b43f3573340aad30bf1e33ccbb3
Scott Clampet <scottc@ifit.com>
2020-09-28 10:42:26 -0600

    Add SquareRt

---
+++ SquareRt/SquareRt.Tests/SquareRtCalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+using SquareRt.Core;
+
+namespace SquareRt.Tests
+{
+    [TestFixture]
+    public class SquareRtCalculatorTests
+    {
+        ISquareRtCalculator _calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calc = new SquareRtCalculator();
+        }
+
+        [TestCase(0)]
+        [TestCase(2)]
+        [TestCase(-1)]
+        public void Calculate(int num)
+        {
+            _calc.Calculate(num).ShouldBe(Math.Sqrt(num));
+        }
+
+    }
+}
